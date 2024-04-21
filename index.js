a579302f6732ff356dd2837a124feffd3565d765
azharejaz7 <azharejaz7@gmail.com>
2024-04-22 03:33:48 +0500

    add shebang

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
-#! usr/bin/env node
+#! /usr/bin/env node
 import inquirer from "inquirer";
-let currency = {
+// Currency object
+const currency = {
     PKR: 277.69,
     USD: 1, // base currency
     EUR: 0.83, // Example exchange rate, actual rate may vary
     GBP: 0.72,
     INR: 83.37,
 };
+// take Currency Input
 let CurrencyDataAnswer = await inquirer.prompt([
     {
-        name: 'from',
-        message: 'Convert Currency From :',
+        name: "from",
+        message: "Convert Currency From :",
         type: "list",
-        choices: ['USD', 'PKR', 'EUR', 'GBP', 'INR']
+        choices: ["USD", "PKR", "EUR", "GBP", "INR"],
     },
     {
-        name: 'to',
-        message: 'Convert Currency From :',
+        name: "to",
+        message: "Convert Currency From :",
         type: "list",
-        choices: ['USD', 'PKR', 'EUR', 'GBP', 'INR']
+        choices: ["USD", "PKR", "EUR", "GBP", "INR"],
     },
     {
-        name: 'amount',
-        message: 'Emter Amount :',
+        name: "amount",
+        message: "Emter Amount :",
         type: "number",
-    }
+    },
 ]);
 //Data get
 let fromCurrency = currency[CurrencyDataAnswer.from];
 let toCurrency = currency[CurrencyDataAnswer.to];
+// formulation
 let baseAmt = CurrencyDataAnswer.amount / fromCurrency;
 let convertedAmt = baseAmt * toCurrency;
+//Print values
 console.log(`Your Converted Amount is : ${convertedAmt.toFixed(2)} ${CurrencyDataAnswer.to}`);
-// formulation
